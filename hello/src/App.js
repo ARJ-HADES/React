5bacf7ca97fc673bb6287c7392ddaa9fb8c3ddeb
ARJ-HADES <68515819+ARJ-HADES@users.noreply.github.com>
2021-08-10 22:05:57 +0400

    commit on 'master'

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Greet from './components/greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
+import Message from './components/Message';
+import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
-     <Greet name="Arjun" fullName="Arjun vinod">
+      <Message />
+      <Counter />
+     {/* <Greet name="Arjun" fullName="Arjun vinod">
        <p>
          His age is 25 props
        </p>
@@ -18,7 +21,7 @@ function App() {
      <Welcome name="Lalu" fullName="Sourabh Aniyeri"/>
      <Welcome name="Savya" fullName="Savayasach"/>
      <Welcome name="Sreekuttan" fullName="Sreehari"/>
-     {/* <Hello /> */}
+     <Hello /> */}
     </div>
   );
 }
