d8b1cad1f589d2bb71f0592c36a1daeae00888ea
ARJ-HADES <68515819+ARJ-HADES@users.noreply.github.com>
2021-08-12 15:44:22 +0400

    commit on master

--- hello/src/components/Counter.js
+++ hello/src/components/Counter.js
@@ -16,24 +16,36 @@ constructor(props) {
 
 increment(){
 
-    this.setState({
-        count:this.state.count + 1 
-    },
-    ()=>{
-        console.log('callback value', this.state.count)
-    }
+    // this.setState({
+    //     count:this.state.count + 1 
+    // },
+    // ()=>{
+    //     console.log('callback value', this.state.count)
+    // }
     
-    )
+    // )
+
+    this.setState((prevState)=>({
+        count: prevState.count + 1 
+    }))
     console.log(this.state.count)
+
 }
 
+incrementFive (){
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
+}
 
 
     render() {
         return (
             <div>
             <div> Count - {this.state.count}</div>
-            <button onClick={()=>this.increment()}>Increment</button>
+            <button onClick={()=>this.incrementFive()}>Increment</button>
             </div>
         )
     }
